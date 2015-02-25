e8d5333e2518388f68604bf1d231af40c4ffa35a
Kyle Mitchell <kyle@kemitchell.com>
2015-02-25 00:41:13 +0000

    test file organization; corrections

--- test/validation.test.js
+++ test/validation.test.js
@@ -1,5 +1,7 @@
 /* jshint mocha: true */
 var Validator = require('jsonschema').Validator;
+var romanize = require('romanize');
+var path = require('path');
 var expect = require('chai').expect;
 var glob = require('glob');
 var jsonFiles = glob.sync('data/**/*.json');
@@ -12,10 +14,24 @@ var schema = require('../schemas/provision.json');
 describe('files', function() {
   jsonFiles.forEach(function(jsonFile) {
     describe(jsonFile, function() {
+      var json = require('../' + jsonFile);
+      var parsedPath = path.parse(jsonFile);
+
       it('validates', function() {
-        var json = require('../' + jsonFile);
         expect(validator.validate(json, schema).errors)
           .to.be.eql([]);
+      });
+
+      it('is correctly numbered', function() {
+        expect(json.provision[json.provision.length - 1].number)
+          .to.equal(parsedPath.base.replace('.json',''));
+      });
+
+      it('is in the right directory', function() {
+        var dir = parsedPath.dir;
+        var containingDir = dir.slice(dir.lastIndexOf(/\/|\\/) - 1);
+        expect(json.provision[json.provision.length - 2].number)
+          .to.equal(romanize(parseInt(containingDir)));
       });
     });
   });
