50f5d9fbd00f2f84ec74a606fd90c23944798de1
Kyle Mitchell <kyle@kemitchell.com>
2015-02-25 01:33:34 +0000

    build Markdown files

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 all:
 	npm install
 	find data -iname "*.json" | xargs -I FILE ./node_modules/.bin/json -I -f FILE
+	find data -iname "*.json" | xargs -I FILE node ./build/tomarkdown.js FILE
---
+++ build/tomarkdown.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+var jsonFile = process.argv[2];
+var json = JSON.parse(fs.readFileSync(jsonFile));
+
+var out = fs.createWriteStream(jsonFile.replace(/\.json$/,'.md'));
+
+var indentation = function(level) {
+  return level < 1 ? '' : new Array((level * 2) + 1).join(' ');
+};
+
+var renderElement = function(depth, element) {
+  if ('type' in element) {
+    out.write('\n\n' + indentation(depth) + '- ');
+    if (element.type === 'blockquote') {
+      out.write('> ');
+    }
+    out.write(element.text);
+  } else if ('number' in element) {
+    out.write('\n\n' + indentation(depth));
+    out.write('- ' + element.number + ' ');
+    if (typeof element.content === 'string') {
+      out.write(element.content);
+    } else {
+      element.content.forEach(renderElement.bind(this, depth + 1));
+    }
+  }
+};
+
+out.write('# Section ' + json.provision[json.provision.length - 1].number);
+out.write('\n\n' + json.heading + '.');
+json.content.forEach(renderElement.bind(this, 0));
